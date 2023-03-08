cbe5fc5c5844d2c6ece9301d996bf866d12ed58f
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-03-08 21:35:54 +1300

    lesson15

---
+++ js/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+};
+
+DomElement.prototype.render = function () {
+    let elem;
+    if (this.selector.startsWith('.')) {
+        elem = document.createElement('div');
+        elem.classList.add(this.selector.slice(1));
+    } else if (this.selector.startsWith('#')) {
+        elem = document.createElement('p');
+        elem.setAttribute('id', this.selector.slice(1));
+    } else {
+        elem = document.createElement('div');
+    }
+
+    elem.style.cssText = `
+      height: ${this.height};
+      width: ${this.width};
+      background-color: ${this.bg};
+      font-size: ${this.fontSize};
+    `;
+
+    elem.textContent = 'Hi, mate!';
+
+    document.body.appendChild(elem);
+};
+
+const myElement = new DomElement('#greet', '100px', '200px', 'turquoise', '30px');
+myElement.render();
+
+
